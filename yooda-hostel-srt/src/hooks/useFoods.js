10fda4188776adca89e3ea2985e1dc4e08ce713f
SR TAMIM <saifur.tamim@gmail.com>
2022-02-09 12:11:12 +0600

    minor changes

--- yooda-hostel-srt/src/hooks/useFoods.js
+++ yooda-hostel-srt/src/hooks/useFoods.js
@@ -1,15 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const useFoods = () => {
-    const [foods, setFoods] = useState([])
-
-    useEffect(() => {
-        const getFoods = [
-            { id: 1, foodName: 'Burger', foodPrice: 99 },
-            { id: 2, foodName: 'Pizza', foodPrice: 149 }
-        ]
-        setFoods(getFoods)
-    }, [])
+    const [foods, setFoods] = useState([
+        { id: 1, foodName: 'Burger', foodPrice: 99 },
+        { id: 2, foodName: 'Pizza', foodPrice: 149 }
+    ])
 
     return { foods }
 };
