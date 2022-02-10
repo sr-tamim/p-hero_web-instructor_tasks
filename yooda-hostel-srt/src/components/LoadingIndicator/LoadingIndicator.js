6378b9b5c92f18d477e14182f2334159a2566700
TAMIM <saifur.tamim@gmail.com>
2022-02-10 22:39:00 +0600

    loading spinner position fixed

--- yooda-hostel-srt/src/components/LoadingIndicator/LoadingIndicator.js
+++ yooda-hostel-srt/src/components/LoadingIndicator/LoadingIndicator.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingIndicator = () => {
     return (
-        <div className='d-flex justify-content-center align-items-center position-absolute left-0 top-0 w-100 h-100'
+        <div className='d-flex justify-content-center align-items-center position-fixed left-0 top-0 w-100 h-100'
             style={{ background: 'rgba(0, 0, 0, 0.4)' }}>
             <div className="spinner-grow text-primary" role="status"
                 style={{ width: '3rem', height: '3rem' }}></div>
